06bb98d300ccf2e195c28010d3e397418415a919
Abe Darwish <idarwishman@gmail.com>
2020-01-30 13:49:50 -0500

    Update FETCH_vehicle_specifications.js

--- Vehicle-Specifications/FETCH_vehicle_specifications.js
+++ Vehicle-Specifications/FETCH_vehicle_specifications.js
@@ -6,9 +6,9 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-var vin = "5NPET4AF9AH591666";
-var apiKey = "brneiz7rk_jw8rp07wl_609srodys";
-var includeData = "selections,attributes,equipment,colors,recalls,warranties,photos"
+var vin = "5NPET4AF9AH591666"; // replace with the VIN you'd like to look up
+var apiKey = "brneiz7rk_jw8rp07wl_609srodys"; // replace with your own API key
+var includeData = "selections,attributes,equipment,colors,recalls,warranties,photos"; // remove items you don't need (or keep them all)
 
 fetch(`https://api.carsxe.com/specs?vin=${vin}&key=${apiKey}&include=${includeData}`, requestOptions)
   .then(response => response.text())
